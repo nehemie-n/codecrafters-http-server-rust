27f5e2d5bea621217ad228bad10520f9910bc2a3
nehemie-n <nehemie054@gmail.com>
2024-04-23 16:03:22 +0200

    feat: testing 404

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,48 @@
-// Uncomment this block to pass the first stage
+use std::io::BufRead;
+use std::io::BufReader;
+use std::io::Read;
 use std::io::Write;
 use std::net::TcpListener;
+use std::net::TcpStream;
+
+/**
+ * GET /index.html HTTP/1.1
+ *
+ * Host: localhost:4221
+ * User-Agent: curl/7.64.1
+ */
+fn handle_request(mut stream: TcpStream) {
+    println!("new client!");
+    let mut reader = BufReader::new(&mut stream);
+    let mut first_line = String::new();
+    reader.read_line(&mut first_line).unwrap();
+
+    println!("first line: {}", first_line);
+    let parts = first_line.split_whitespace().collect::<Vec<&str>>();
+    let path = parts[1];
+
+    match path {
+        "/" => {
+            let resp = "HTTP/1.1 200 OK\r\n\r\n".to_string();
+            let _ = stream.write(resp.as_bytes());
+        }
+        "/index.html" => {
+            let resp = "HTTP/1.1 404 NOT FOUND\r\n\r\n".to_string();
+            let _ = stream.write(resp.as_bytes());
+        }
+        _ => {
+            let resp = "HTTP/1.1 404 NOT FOUND\r\n\r\n".to_string();
+            let _ = stream.write(resp.as_bytes());
+        }
+    }
+}
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-    //
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    //
     for stream in listener.incoming() {
-        match stream {
-            Ok(mut _stream) => {
-                let _ = _stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes());
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        let stream = stream.unwrap();
+        handle_request(stream);
+        print!("new client!");
     }
 }
