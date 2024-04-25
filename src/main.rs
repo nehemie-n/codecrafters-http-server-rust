92fd485a008ac8bad176b21e248b212c0946028d
nehemie-n <nehemie054@gmail.com>
2024-04-25 15:54:28 +0200

    feat: updates

--- src/main.rs
+++ src/main.rs
@@ -93,10 +93,14 @@ fn main() {
     println!("Logs from your program will appear here!");
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     for stream in listener.incoming() {
-        thread::spawn(move || {
-            let stream = stream.unwrap();
-            handle_request(stream);
-        });
-        println!("new client!");
+        match stream {
+            Ok(stream) => {
+                thread::spawn(move || {
+                    handle_request(stream);
+                });
+                println!("new client!");
+            }
+            Err(_) => {}
+        }
     }
 }
